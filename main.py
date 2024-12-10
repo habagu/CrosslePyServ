cb17bf1cad0a69f07e1a31780fcd199314e67723
Suit-Ji <suit.it.pub@gmail.com>
2024-12-10 10:34:21 +0100

    added startup options

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 
 from crossle_server import start_server
+import sys
+from analyze import analyze_image
+import os
 
-start_server()
+def main():
+    mode = input("Enter mode (debug/server): ").strip().lower()
+    os.system('cls' if os.name == 'nt' else 'clear')
+    if mode == 'debug':
+        analyze_image()
+    else:
+        start_server()
+
+if __name__ == "__main__":
+    main()
