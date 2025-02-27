cd8731ad887c95e9d4438b98386a21a4b9dbde8d
habagu <joshua.naumann49@gmail.com>
2025-02-27 11:33:34 +0100

    fix

--- neuro.py
+++ neuro.py
@@ -96,7 +96,7 @@ def learn():
     for chunk in reader:
         loading += chunksize  # Track actual rows loaded
         progress_print("Loaded Data: " + str(loading)) # Print progress
-        chunk = shuffle(chunk, random_state=42)[:int(len(chunk)*0.9)]
+        chunk = shuffle(chunk, random_state=42)[:int(len(chunk)*0.8)]
         chunks.append(chunk)  # Store chunk
 
     # Combine all chunks into a single DataFrame
